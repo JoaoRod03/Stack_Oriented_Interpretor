81d02be6d826c0a1473a9fec63fdea657a2508e8
a100837 <a100837@alunos.uminho.pt>
2022-04-24 19:55:45 +0100

    Limpeza de codigo e novas funções

--- code/funcoes.c
+++ code/funcoes.c
@@ -15,47 +15,70 @@ STACK* nova() {
     return ((STACK *) malloc(sizeof(STACK)));
 }
 
-/// Função que insere um elemento x no topo da stack. (PUSH de ints)
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  PUSH'S  <-----|/\/\/\/\/\/\/\/\/\/\/\/
+
+/// Função que insere um elemento do tipo long no topo da stack. 
 void push(STACK *s,long x) {
     s->topo++;
-    s->pilha[s->topo].tipo = tLong;  // Indica que é long
-    s->pilha[s->topo].val.l = x;  // Push para o campo int
+    s->pilha[s->topo].tipo = tLong;  // Atribui o tipo long ao topo da stack
+    s->pilha[s->topo].val.l = x;     // Push para o campo long
 }
 
-/// Função que insere um elemento x no topo da stack. (PUSH de doubles)
+/// Função que insere um elemento do tipo double no topo da stack.
 void push_double(STACK *s,double x) {
     s->topo++;
-    s->pilha[s->topo].tipo = tDouble;   // Indica que é double
-    s->pilha[s->topo].val.d = x;  // Push para o campo double
+    s->pilha[s->topo].tipo = tDouble;   // Atribui o tipo double ao topo da stack
+    s->pilha[s->topo].val.d = x;        // Insere o elemento x no topo da stack
 }
 
-/// Função que insere um elemento x no topo da stack. (PUSH de chars)
+/// Função que insere um elemento do tipo char no topo da stack. 
 void push_char(STACK *s ,char x) {
     s->topo++;
-    s->pilha[s->topo].tipo = tChar;  // Indica que é char
-    s->pilha[s->topo].val.c = x;
+    s->pilha[s->topo].tipo = tChar;  // Atribui o tipo char ao topo da stack
+    s->pilha[s->topo].val.c = x;     // Insere o elemento x no topo da stack
 }
 
-/// Função que retira o elemento que se encontra no topo da stack. (Indentifica primeiro o tipo do elemento)
+/// Função que insere um elemento do tipo string no topo da stack. 
+void push_string(STACK *s ,char* x) {
+    s->topo++;
+    s->pilha[s->topo].tipo = tStr;   // Atribui o tipo string ao topo da stack
+    s->pilha[s->topo].val.s = x;     // Insere o elemento x no topo da stack
+}
+
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  POP'S  <-----|/\/\/\/\/\/\/\/\/\/\/\/
+
+/// Função que retira o elemento do tipo long que se encontra no topo da stack.
 long pop(STACK *s) {
-        long v = s->pilha[s->topo].val.l;
-        s->topo--;
+        long v = s->pilha[s->topo].val.l;  // Obtem o long no topo da stack 
+        s->topo--;  
         return v ; 
 }
 
+/// Função que retira o elemento do tipo double que se encontra no topo da stack.
 double pop_double(STACK *s) {
-        double v = s->pilha[s->topo].val.d;
+        double v = s->pilha[s->topo].val.d;  // Obtem o double no topo da stack 
         s->topo--;
         return v ;
 }
 
+/// Função que retira o elemento do tipo char que se encontra no topo da stack.
 char pop_char(STACK *s) {
-        char v = s->pilha[s->topo].val.c;
+        char v = s->pilha[s->topo].val.c;  // Obtem o char no topo da stack 
         s->topo--;
         return v ;
 }
 
-/** Função responsavel por associar um token de um operador á sua função correspondente
+char* pop_string(STACK *s) {
+        char v = s->pilha[s->topo].val.s;  // Obtem a string no topo da stack 
+        s->topo--;
+        return v ;
+}
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  OUTROS  <-----|/\/\/\/\/\/\/\/\/\/\/\/
+
+/** Função responsavel por associar um token de um operador á sua função correspondente.
 *   @param [in] Stack
 *   @param [in] Token
 */ 
@@ -85,7 +108,7 @@ void handle (STACK *s,char *token){
     num (s,token)) {return;} // Deixar no fim
 }
 
-/// Verifica se não há nenhum operador no input recebido/token desconhecido. Trata das constantes.
+/// Função que trata dos tokens não reconhecidos e constantes, verifica o tipo do token e atribui uma operação.
 int num (STACK *s,char *token) {
     long val;
     double val2;
@@ -110,6 +133,9 @@ int topo_tipo (STACK *s) {
     r = s->pilha[s->topo].tipo; 
     return r;
 }
+
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  OPERAÇÕES  <-----|/\/\/\/\/\/\/\/\/\/\/\/
 
 /// Função que soma dois elementos da stack e coloca o resultado no topo desta.
 int soma (STACK *s,char *token) {
@@ -232,11 +258,10 @@ int expo (STACK *s,char *token) {
     return 0;
 }
 
-/// Incrementa o valor de um elemento da stack.
+/// Incrementa o valor de um elemento da stack em 1 valor.
 int incrementa (STACK *s,char *token) {
     if(strcmp(token, ")") == 0) {
-
-        int i1=0,t1=0;
+        int i1=0, t1=0;
         double d1=0.0;
         char c1=' ';
 
@@ -258,10 +283,10 @@ int incrementa (STACK *s,char *token) {
     return 0 ;
 }
 
-/// Decrementa o valor de um elemento da stack.
+/// Decrementa o valor de um elemento da stack em 1 valor.
 int decrementa (STACK *s,char *token) {
     if(strcmp(token, "(") == 0) {
-        int i1=0,t1=0;
+        int i1=0, t1=0;
         double d1=0.0;
         char c1=' ';
 
@@ -283,20 +308,8 @@ int decrementa (STACK *s,char *token) {
     return 0 ;
 }
 
-// NAO UTILIZADO: Transforma um numero decimal em um numero binario
-/*
-long binario (int x) {
-	long r=0;
-    int i=0,resto=0;
-	while (x>=1) {
-		resto=x%2;
-		r=r+(resto*pow(10,i));
-		x=(x-resto)/2;
-		i++;
-	}
-	return r;
-} 
-*/
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  OPERAÇÕES BITWISE  <-----|/\/\/\/\/\/\/\/\/\/\/\/
 
 /// Realiza a operação (and / &) ao nivel binario entre dois elementos da stack, coloca o resultado final nesta.
 int logica_and (STACK *s,char *token) {
@@ -341,8 +354,10 @@ int logica_not (STACK *s,char *token) {
     return 0;
 }
 
-// GUIAO 2    ------------------------------------------- //
-/// Duplica o elemento no topo da Stack 
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  OPERAÇÕES  <-----|/\/\/\/\/\/\/\/\/\/\/\/
+
+/// Duplica o elemento que se encontra no topo da stack. 
 int duplicar (STACK *s,char *token) {
     if(strcmp(token, "_") == 0) {
         long i1=0,t1=0;
@@ -363,23 +378,26 @@ int duplicar (STACK *s,char *token) {
     return 0;
 }
 
-/// Roda os 3 elementos no topo da stack
+/// Roda os 3 elementos no topo da stack (123 -> 231).
 int rodar (STACK *s,char *token) {
     if(strcmp(token, "@") == 0) {
         int i1=0, i2=0, i3=0, t1=0, t2=0, t3=0;
         double d1=0.0, d2=0.0, d3=0;
         char c1=' ',c2=' ',c3=' ';
 
+        // Retira C
         t3=topo_tipo(s);
         if (t3==tDouble) {d3=pop_double(s);}
         else if (t3==tLong) {i3=pop(s);}
         else if (t3==tChar) {c3=pop_char(s);}
 
+        // Retira B
         t2=topo_tipo(s);
         if (t2==tDouble) {d2=pop_double(s);}
         else if (t2==tLong) {i2=pop(s);}
         else if (t2==tChar) {c2=pop_char(s);}
 
+        // Retira A
         t1=topo_tipo(s);
         if (t1 == tDouble) {d1=pop_double(s);}
         else if (t1==tLong) {i1=pop(s);}
@@ -387,30 +405,27 @@ int rodar (STACK *s,char *token) {
 
         /////////////////////////////////////////////
 
-        // C
+        // Adiciona B
         if (t2==tLong) {push(s,i2);}
         if (t2==tDouble) {push_double(s, d2);}
         if (t2==tChar) {push_char(s, c2);}
 
-        // B
+        // Adiciona C
         if (t3==tLong) {push(s,i3);}
         if (t3==tDouble) {push_double(s, d3);}
         if (t3==tChar) {push_char(s, c3);}
 
-
-        // A
+        // Adiciona A
         if (t1==tLong) {push(s,i1);}
         if (t1==tDouble) {push_double(s, d1);}
         if (t1==tChar) {push_char(s, c1);}
-
-
 
         return 1;
     }
     return 0;
 }
 
-/// Dá Pop no topo da Stack
+/// Retira o elemento que se encontra no topo da stack.
 int pop_elem (STACK *s,char *token) {
     if(strcmp(token, ";") == 0) {
         int t1;
@@ -421,33 +436,33 @@ int pop_elem (STACK *s,char *token) {
     return 0;
 }
 
-
 /// Troca os dois elementos do topo da stack
 int trocar (STACK *s,char *token) {
     if(strcmp(token, "\\") == 0) {
-        long i1=0,i2=0,t1=0,t2=0;
-        double d1=0.0,d2=0.0;
-        char c1=' ',c2=' ';
+        long i1=0, i2=0, t1=0, t2=0;
+        double d1=0.0, d2=0.0;
+        char c1=' ', c2=' ';
 
-        t1=topo_tipo(s);
+        // Retira A
+        t1=topo_tipo(s); 
         if (t1 == tDouble) {d1=pop_double(s);}
         else if (t1==tLong) {i1=pop(s);}
         else if (t1==tChar) {c1=pop_char(s);}
 
-        t2=topo_tipo(s);
+        // Retira B
+        t2=topo_tipo(s); 
         if (t2==tDouble) {d2=pop_double(s);}
         else if (t2==tLong) {i2=pop(s);}
         else if (t2==tChar) {c2=pop_char(s);}
 
-
         /////////////////////////////////////////////
 
-        // A
+        // Adiciona A
         if (t1==tLong) {push(s,i1);}
         if (t1==tDouble) {push_double(s, d1);}
         if (t1==tChar) {push_char(s, c1);}
 
-        // B
+        // Adiciona B
         if (t2==tLong) {push(s,i2);}
         if (t2==tDouble) {push_double(s, d2);}
         if (t2==tChar) {push_char(s, c2);}
@@ -457,8 +472,7 @@ int trocar (STACK *s,char *token) {
     return 0;
 }
 
-
-///  Copia n-ésimo elemento para o topo da stack
+///  Copia n-ésimo elemento para o topo da stack. (n $)
 int copia (STACK *s,char *token) {
     if(strcmp(token, "$") == 0) {
         int w,ultimo;
@@ -485,7 +499,10 @@ int copia (STACK *s,char *token) {
     return 0;
 }
 
-/// Converte o topo da stack para int
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  CONVERSÕES  <-----|/\/\/\/\/\/\/\/\/\/\/\/
+
+/// Converte o topo da stack para int.
 int conv_int (STACK *s,char *token) {
     if (strcmp(token, "i") == 0) {
         if (topo_tipo(s) == tLong) { 
@@ -511,7 +528,7 @@ int conv_int (STACK *s,char *token) {
     return 0;
 }
 
-/// Converte o topo da stack para double
+/// Converte o topo da stack para double.
 int conv_double (STACK *s,char *token) {
     if (strcmp(token, "f") == 0) {
         if (topo_tipo(s) == tLong) { 
@@ -530,7 +547,7 @@ int conv_double (STACK *s,char *token) {
     return 0;
 }
 
-/// Converte o topo da stack para char
+/// Converte o topo da stack para char.
 int conv_char (STACK *s,char *token) {
     if (strcmp(token, "c") == 0) {
         if (topo_tipo(s) == tLong) { 
@@ -540,7 +557,7 @@ int conv_char (STACK *s,char *token) {
             push_char (s,r);
         }
 
-        if (topo_tipo(s)  == tDouble) { 
+        if (topo_tipo(s) == tDouble) { 
             double aux = pop_double(s);
             int x = (int) aux;
             char r = (char) x;
@@ -552,6 +569,10 @@ int conv_char (STACK *s,char *token) {
     return 0;
 }
 
+
+//\/\/\/\/\/\/\/\/\/\/\/|---->  OUTROS  <-----|/\/\/\/\/\/\/\/\/\/\/\/
+
+/// Função que lê input e insere o resultado desse input na stack.
 int ler (STACK *s, char *token) {
     char line [BUFSIZ] ; 
     if (strcmp(token, "l") == 0) {
@@ -567,11 +588,8 @@ int ler (STACK *s, char *token) {
     return 0;
 }
 
-//-------------------------------------------
-//-------------------------------------------
-
-/// Converte o topo da stack para string
 /*
+/// Converte o topo da stack para string
 int conv_string (STACK *s,char *token) {
     if(strcmp(token, "s") == 0) {
         if (s->pilha[s->topo].tipo == 0) { 
@@ -589,25 +607,17 @@ int conv_string (STACK *s,char *token) {
     }
     return 0;
 }
-*/
-//---------------------------------------
-//---------------------------------------
 
-
-
-
-
-
-
-// Prototipos ------------------------------------------- //
-// Outros guiões ---------------------------------------- //
-/*
-int ler (STACK *s,char *token) {
-    if(strcmp(token, "l") == 0) {
-        STACK *s = nova2();
-        input(nova2); // Temos de meter o codigo do input da main numa função chamada input e que recebe a stack onde realizar as operações
-        return 1;
-    }
-    return 0;
-}
+// NAO UTILIZADO: Transforma um numero decimal em um numero binario
+long binario (int x) {
+	long r=0;
+    int i=0,resto=0;
+	while (x>=1) {
+		resto=x%2;
+		r=r+(resto*pow(10,i));
+		x=(x-resto)/2;
+		i++;
+	}
+	return r;
+} 
 */
--- code/tipos.h
+++ code/tipos.h
@@ -9,74 +9,87 @@
 // Configs
 #define STACK_SIZE 1000 ///< Constante que define o tamanho da stack
 
-////////////////////////////////////////////////
+/////////////////// TIPOS ///////////////////
 
+/// Enum que facilita a leitura de codigo.
 typedef enum {
-    tLong,      // Valor 0
-    tDouble,    // Valor 1
-    tChar,      // Valor 2
-    tStr        // Valor 3
+    tLong,    // Valor 0
+    tDouble,  // Valor 1
+    tChar,    // Valor 2
+    tStr      // Valor 3
 } Enum_tipo;
 
+/// União responsavel por oferecer á stack varios tipos.
 typedef union un {
-    long l;
-    double d;
-    char c;
-    char s[BUFSIZ]; 
+    long l;         // Long
+    double d;       // Double
+    char c;         // Char
+    char s[BUFSIZ]; // String
 } Uniao_tipos;
 
+/// Struct que define o tipo dos elementos da stack (Possuem uma união de tipos e um enum que indica o tipo do elemento)
 typedef struct ru {
     Enum_tipo tipo;
     Uniao_tipos val;
 } reuniao;
 
+/// Struct da stack, constituido por um array que armazena os valores da stack e um apontador para o topo desta.
 typedef struct stack {
     reuniao pilha [STACK_SIZE]; 
     int topo; 
 } STACK;
 
-////////////////////////////////////////////////
+/////////////////// INICIALIZAÇÃO DE FUNÇÕES ///////////////////
 
-// Iniciar funções
+// Iniciar stack
 STACK* nova();
 
 // PUSH'S
-void push(STACK *s,long x);
-void push_double(STACK *s,double x);
-void push_char(STACK *s,char x);
+void push (STACK *s,long x);
+void push_double (STACK *s,double x);
+void push_char (STACK *s,char x);
+void push_string (STACK *s,char* x);
 
 // POP'S
-long pop(STACK *s);
-double pop_double(STACK *s);
-char pop_char(STACK *s);
+long pop (STACK *s);
+double pop_double (STACK *s);
+char pop_char (STACK *s);
+char* pop_string (STACK *s);
 
 // OUTROS
-void handle(STACK *s,char *token);
-int num(STACK *s,char *token);
+void handle (STACK *s,char *token);
+int num (STACK *s,char *token);
 int top_tipo (STACK *s);
 
 // OPERAÇÕES
-int soma(STACK *s,char *token);
-int menos(STACK *s,char *token);
-int mul(STACK *s,char *token);
-int divide(STACK *s,char *token);
-int resto(STACK *s,char *token);
-int expo(STACK *s,char *token);
-int incrementa(STACK *s,char *token);
-int decrementa(STACK *s,char *token);
-int logica_and(STACK *s,char *token);
-int logica_or(STACK *s,char *token);
-int logica_xor(STACK *s,char *token);
-int logica_not(STACK *s,char *token);
-int duplicar(STACK *s,char *token);
+int soma (STACK *s,char *token);
+int menos (STACK *s,char *token);
+int mul (STACK *s,char *token);
+int divide (STACK *s,char *token);
+int resto (STACK *s,char *token);
+int expo (STACK *s,char *token);
+int incrementa (STACK *s,char *token);
+int decrementa (STACK *s,char *token);
+
+// BITWISE
+int logica_and (STACK *s,char *token);
+int logica_or (STACK *s,char *token);
+int logica_xor (STACK *s,char *token);
+int logica_not (STACK *s,char *token);
+
+// OPERAÇÕES 2
+int duplicar (STACK *s,char *token);
 int rodar (STACK *s,char *token);
 int pop_elem (STACK *s,char *token);
 int trocar (STACK *s,char *token);
 int copia (STACK *s,char *token);
+
+// CONVERSÕES
 int conv_int (STACK *s,char *token);
 int conv_double (STACK *s,char *token);
 int conv_char (STACK *s,char *token);
 int ler (STACK *s, char *token);
 // long binario(); Não implementado
+
 
 #endif // TIPOS_H
