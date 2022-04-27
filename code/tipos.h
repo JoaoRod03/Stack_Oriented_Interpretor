23bafe61f7094893ededa3533541f678687e6fbf
a100837 <a100837@alunos.uminho.pt>
2022-04-27 23:04:26 +0100

    CODIGO OTIMIZADO

--- code/tipos.h
+++ code/tipos.h
@@ -31,11 +31,11 @@ typedef union un {
 typedef struct ru {
     Enum_tipo tipo;
     Uniao_tipos val;
-} reuniao;
+} tipos;
 
 /// Struct da stack, constituido por um array que armazena os valores da stack e um apontador para o topo desta.
 typedef struct stack {
-    reuniao pilha [STACK_SIZE]; 
+    tipos pilha [STACK_SIZE]; 
     int topo; 
 } STACK;
 
@@ -44,53 +44,46 @@ typedef struct stack {
 // Iniciar stack
 STACK* nova();
 
-// PUSH'S
-void push (STACK *s,long x);
-void push_double (STACK *s,double x);
-void push_char (STACK *s,char x);
-void push_string (STACK *s,char* x);
+// PUSH e POP
+void push (STACK *s,tipos x); /// Função que insere um elemento no topo da stack. 
+tipos pop (STACK *s); /// Função que retira o elemento que se encontra no topo da stack.
 
-// POP'S
-long pop (STACK *s);
-double pop_double (STACK *s);
-char pop_char (STACK *s);
-char* pop_string (STACK *s);
 
 // OUTROS
-void handle (STACK *s,char *token);
-int num (STACK *s,char *token);
-int top_tipo (STACK *s);
+void handle (STACK *s,char *token); // Função responsavel por associar um token de um operador á sua função correspondente.
+int num (STACK *s,char *token); /// Função que trata dos tokens não reconhecidos e constantes, verifica o tipo do token e atribui uma operação.
+int top_tipo (STACK *s); /// Devolve o tipo do topo da stack
+
 
 // OPERAÇÕES
-int soma (STACK *s,char *token);
-int menos (STACK *s,char *token);
-int mul (STACK *s,char *token);
-int divide (STACK *s,char *token);
-int resto (STACK *s,char *token);
-int expo (STACK *s,char *token);
-int incrementa (STACK *s,char *token);
-int decrementa (STACK *s,char *token);
+int soma (STACK *s,char *token); /// Função que soma dois elementos da stack e coloca o resultado no topo desta.
+int menos (STACK *s,char *token); /// Função que subtrai dois elementos da stack e coloca o resultado no topo desta.
+int mul (STACK *s,char *token); /// Função que multiplica dois elementos da stack e coloca o resultado no topo desta.
+int divide (STACK *s,char *token); /// Função que divide dois elementos da stack e coloca o resultado no topo desta.
+int resto (STACK *s,char *token); /// Função que calcula o resto da divisao de dois elementos da stack e coloca o resultado no topo desta.
+int expo (STACK *s,char *token); /// Calcula o resultado de elevar um elemento da stack a outro, coloca o resultado no topo da stack.
+int incrementa (STACK *s,char *token); /// Incrementa o valor de um elemento da stack em 1 valor.
+int decrementa (STACK *s,char *token); /// Decrementa o valor de um elemento da stack em 1 valor.
 
 // BITWISE
-int logica_and (STACK *s,char *token);
-int logica_or (STACK *s,char *token);
-int logica_xor (STACK *s,char *token);
-int logica_not (STACK *s,char *token);
+int logica_and (STACK *s,char *token); /// Realiza a operação (and / &) ao nivel binario entre dois elementos da stack, coloca o resultado final nesta.
+int logica_or (STACK *s,char *token); /// Realiza a operação (or / |) ao nivel binario entre dois elementos da stack, coloca o resultado final nesta.
+int logica_xor (STACK *s,char *token); /// Realiza a operação (xor / ^) ao nivel binario entre dois elementos da stack, coloca o resultado final nesta.
+int logica_not (STACK *s,char *token); /// Realiza a operação (not / ~) ao nivel binario entre dois elementos da stack, coloca o resultado final nesta.
 
 // OPERAÇÕES 2
-int duplicar (STACK *s,char *token);
-int rodar (STACK *s,char *token);
-int pop_elem (STACK *s,char *token);
-int trocar (STACK *s,char *token);
-int copia (STACK *s,char *token);
+int duplicar (STACK *s,char *token); /// Duplica o elemento que se encontra no topo da stack. 
+int rodar (STACK *s,char *token); /// Roda os 3 elementos no topo da stack (123 -> 231).
+int pop_elem (STACK *s,char *token); /// Retira o elemento que se encontra no topo da stack.
+int trocar (STACK *s,char *token); /// Troca os dois elementos do topo da stack
+int copia (STACK *s,char *token); ///  Copia n-ésimo elemento para o topo da stack. (n $)
 
 // CONVERSÕES
-int conv_int (STACK *s,char *token);
-int conv_double (STACK *s,char *token);
-int conv_char (STACK *s,char *token);
-int ler (STACK *s, char *token);
-int debugger (STACK *s, char *token);
+int conv_int (STACK *s,char *token); /// Converte o topo da stack para int.
+int conv_double (STACK *s,char *token); /// Converte o topo da stack para double.
+int conv_char (STACK *s,char *token); /// Converte o topo da stack para char.
+int ler (STACK *s, char *token); /// Função que lê input e insere o resultado desse input na stack.
+int debugger (STACK *s, char *token); /// Função responsavel pelo debug do programa.
 // long binario(); Não implementado
-
 
 #endif // TIPOS_H
